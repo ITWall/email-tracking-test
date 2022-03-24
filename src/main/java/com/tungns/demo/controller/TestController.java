2ec5887e1d7186d31066055bcfe6d0c062a0206b
Tung Nguyen <tungns@localhost.localdomain>
2022-03-25 01:20:12 +0700

    add track ip

--- src/main/java/com/tungns/demo/controller/TestController.java
+++ src/main/java/com/tungns/demo/controller/TestController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping(value = "ping")
 public class TestController {
     @GetMapping
-    public String ping() {
+    public String ping(HttpServletRequest request) {
         System.out.println("pong");
+        System.out.println(request.getRemoteAddr());
         return "pong";
     }
 }
